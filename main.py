2077aa91e136d7a6299bc810464a5fe81fd00402
TDMwangi <teddymwangi65@gmail.com>
2023-10-20 16:19:14 +0300

    chore: check if the line is valid

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def check_trend_line(support, pivot, slope, y):
     line_vals = slope * np.array(len(y)) + intercept
 
     diffs = line_vals - y
+
+    # Check to see if the line is valid, return -1 otherwise
+    if support and diffs.max() > 1e-5:
+        return -1.0
+    elif not support and diffs.min() < -1e-5:
+        return -1.0
+
+    err = (diffs ** 2.0).sum()
+    return err
