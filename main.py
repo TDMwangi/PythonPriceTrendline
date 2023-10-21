16472f8a619bca2a8191db001e90fac5ef4751d7
TDMwangi <teddymwangi65@gmail.com>
2023-10-21 15:28:01 +0300

    feat: add the optimize_slope() function

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ def check_trend_line(support, pivot, slope, y):
 
     err = (diffs ** 2.0).sum()
     return err
+
+
+def optimize_slope(support, pivot, init_slope, y):
+    slope_unit = (y.max() - y.min() / len(y))
+
+    opt_step = 1.0
+    min_step = 0.0001
+    curr_step = opt_step
+
+    best_slope = init_slope
+    best_err = check_trend_line(support, pivot, init_slope, y)
